f0696f5776c7b1a6b11d75f119e6993bec3e2cfd
IrinaMaziy <glukmush@gmail.com>
2018-12-03 20:08:44 +0200

    Определение страниц

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -12,5 +12,25 @@
 */
 
 Route::get('/', function () {
-    return view('layout');
-});
+    return view('pages.index');
+})->name('home');
+
+Route::get('/products', function () {
+	return view('pages.shop-grid-4-column');
+})->name('products');
+
+Route::get('/single-product', function () {
+	return view('pages.single-product-v1');
+})->name('single-product');
+
+Route::get('/login-and-register', function () {
+	return view('pages.login-and-register');
+})->name('login-and-register');
+
+Route::get('/track-order', function () {
+	return view('pages.track-order');
+})->name('track-order');
+
+Route::get('/blog', function () {
+	return view('pages.blog');
+})->name('blog');
